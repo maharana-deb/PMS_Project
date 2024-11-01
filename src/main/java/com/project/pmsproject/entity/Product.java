97effe19a41c76e738fa5e6b124039d2ddd81772
maharana7 <alterdev13@gmail.com>
2024-11-01 19:28:13 +0530

    updated

---
+++ src/main/java/com/project/pmsproject/entity/Product.java
@@ -0,0 +1,26 @@
+package com.project.pmsproject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+
+}
